6f31280ac0387fc019a16d5a8ee9fc2fe03e09f2
Kailang Fu <kailangfu@gmail.com>
2018-04-06 21:01:02 -0500

    Lexer, Parser, Interpreter

---
+++ FScript-Mono/Interpreter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace FScriptMono {
+	public static class Interpreter {
+		public static float Eval(Node node) {
+			switch (node.type) {
+				case Node.Num:
+					return node.num;
+
+				case Node.Add:
+					return Eval(node.left) + Eval(node.right);
+				case Node.Sub:
+					return Eval(node.left) - Eval(node.right);
+				case Node.Mul:
+					return Eval(node.left) * Eval(node.right);
+				case Node.Div:
+					return Eval(node.left) / Eval(node.right);
+
+				case Node.Pos:
+					return +Eval(node.left);
+				case Node.Neg:
+					return -Eval(node.left);
+
+				default:
+					throw new Exception("Code pass not possible");
+			}
+		}
+
+		public static string Print(Node node) {
+			switch (node.type) {
+				case Node.Num:
+					return node.num.ToString("N1");
+
+				case Node.Add:
+					return string.Format("({0} + {1})", Print(node.left), Print(node.right));
+				case Node.Sub:
+					return string.Format("({0} - {1})", Print(node.left), Print(node.right));
+				case Node.Mul:
+					return string.Format("({0} * {1})", Print(node.left), Print(node.right));
+				case Node.Div:
+					return string.Format("({0} / {1})", Print(node.left), Print(node.right));
+
+				case Node.Pos:
+					return "+" + Print(node.left);
+				case Node.Neg:
+					return "-" + Print(node.left);
+
+				default:
+					throw new Exception("Code pass not possible");
+			}
+		}
+	}
+}
+
---
+++ FScript-Mono/Parser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace FScriptMono {
+	public sealed class Node {
+		public const int Num = 0;
+		public const int Add = 1, Sub = 2, Mul = 3, Div = 4;
+		public const int Pos = 11, Neg = 12;
+
+		public int type;
+		public float num;
+		public Node left, right;
+	}
+
+	public static class Parser {
+		static Node Num(float num) {
+			return new Node{ type = Node.Num, num = num };
+		}
+
+		static Node UnaryOp(int op, Node exp) {
+			return new Node{ type = op + 10, left = exp };
+		}
+
+		static Node BinOp(int op, Node left, Node right) {
+			return new Node{ type = op, left = left, right = right };
+		}
+
+		static Node Factor() {
+			var token = Lexer.Expect(Token.Num, Token.LParen, Token.Plus, Token.Minus);
+			if (token.type == Token.Num) return Num(token.num);
+			if (token.type == Token.LParen) {
+				var result = Expr();
+				Lexer.Expect(Token.RParen);
+				return result;
+			}
+
+			// Plus || Minus
+			return UnaryOp(token.type, Factor());
+		}
+
+		static Node Term() {
+			var node = Factor();
+			Token token;
+			while ((token = Lexer.Prefer(Token.Mul, Token.Div)).type != Token.Mis) {
+				node = BinOp(token.type, node, Factor());
+			}
+			return node;
+		}
+
+		public static Node Expr() {
+			var node = Term();
+			Token token;
+			while ((token = Lexer.Prefer(Token.Plus, Token.Minus)).type != Token.Mis) {
+				node = BinOp(token.type, node, Term());
+			}
+			return node;
+		}
+	}
+}
+
